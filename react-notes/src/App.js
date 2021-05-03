850be47766641506a715f0b53f35e0805dda09d1
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-02 19:31:20 -0500

    notes start

---
+++ react-notes/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Notes from "./pages/Notes";
+
+export default function BasicExample() {
+  return (
+    <Router>
+
+      <>
+
+        <Switch>
+
+          <Route exact path="/" component={Notes} />
+
+        </Switch>
+
+      </>
+      
+      </Router>
+  );
+}
